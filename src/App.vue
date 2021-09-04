bb3ba29616b2c9913ee826bbc915e5753c0ee360
tealicious <carterdanielr@gmail.com>
2021-09-04 14:57:19 -0400

    start connecting service layer to UX

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,57 @@
 <template>
-<div>
-  <h2>Selected Country: {{selectedCountry}}</h2>
-  <app-select
-    :options="countryOptions"
-    v-model="selectedCountry"
-    name="countrySelect"
-    label="select a country"
-  />
+  <div>
+    <h2>Selected Country: {{ selectedCountry }}</h2>
+    <app-select
+      :options="countryOptions"
+      v-model="selectedCountry"
+      :disabled="!allowCountrySelect"
+      name="countrySelect"
+      :label="countrySelectLabel"
+    />
 
-  <h3>Selected State: {{selectedState}}</h3>
-  <app-select
-    :options="stateOptions"
-    v-model="selectedState"
-    disabled
-    name="stateSelect"
-    label="select a state"
-  />
+    <h3>Selected State: {{ selectedState }}</h3>
+    <app-select
+      :options="stateOptions"
+      v-model="selectedState"
+      :disabled="!allowStateSelect"
+      name="stateSelect"
+      :label="stateSelectLabel"
+      :message="stateSelectMessage"
+    />
 
-  <h3>Selected City: {{selectedcity}}</h3>
-  <app-select
-    :options="cityOptions"
-    v-model="selectedcity"
-    disabled
-    name="citySelect"
-    label="select a city"
-  />
+    <h3>Selected Cities: {{ selectedcity }}</h3>
+    <app-select
+      :options="cityOptions"
+      v-model="selectedcity"
+      :disabled="!allowCitySelect"
+      name="citySelect"
+      :label="citySelectLabel"
+      :message="citySelectMessage"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { getCountries, getStates, getCities } from './ts/services';
-import { countryOptionsFactory, stateOptionsFactory, cityOptionsFactory } from './ts/utils';
+import {
+  countryOptionsFactory,
+  stateOptionsFactory,
+  cityOptionsFactory,
+} from './ts/utils';
 import AppSelect from './components/AppSelect.vue';
 
 export default defineComponent({
   name: 'App',
   data() {
     return {
+      fetchingCountries: true,
       countryOptions: [] as string[],
       selectedCountry: '',
+      fetchingStates: false,
       stateOptions: [] as string[],
       selectedState: '',
+      fetchingCities: false,
       cityOptions: [] as string[],
       selectedcity: '',
     };
@@ -49,26 +59,111 @@ export default defineComponent({
   components: {
     AppSelect,
   },
-  async mounted() {
-    const countries = await getCountries;
-    this.countryOptions = countryOptionsFactory(countries);
-
-    const states = await getStates('United States');
-    this.stateOptions = stateOptionsFactory(states);
-
-    const cities = await getCities('Ohio');
-    this.cityOptions = cityOptionsFactory(cities);
+  watch: {
+    selectedCountry(newVal: string) {
+      if (newVal) {
+        this.fetchStateOptions(newVal);
+      }
+    },
+    selectedState(newVal: string) {
+      if (newVal) {
+        this.fetchCityOptions(newVal);
+      }
+    },
+  },
+  methods: {
+    resetStatesState(): void {
+      this.selectedState = '';
+      this.stateOptions = [];
+    },
+    resetCitiesState(): void {
+      this.selectedcity = '';
+      this.cityOptions = [];
+    },
+    resetAllState(): void {
+      this.resetStatesState();
+      this.resetCitiesState();
+    },
+    async fetchCountryOptions(): Promise<void> {
+      const countries = await getCountries;
+      this.countryOptions = countryOptionsFactory(countries);
+      this.fetchingCountries = false;
+    },
+    async fetchStateOptions(country: string): Promise<void> {
+      this.resetAllState();
+      this.fetchingStates = true;
+      const states = await getStates(country);
+      this.stateOptions = stateOptionsFactory(states);
+      this.fetchingStates = false;
+    },
+    async fetchCityOptions(state: string): Promise<void> {
+      this.resetCitiesState();
+      this.fetchingCities = true;
+      const cities = await getCities(state);
+      this.cityOptions = cityOptionsFactory(cities);
+      this.fetchingCities = false;
+    },
+  },
+  computed: {
+    allowCountrySelect(): boolean {
+      return this.countryOptions.length > 0;
+    },
+    countrySelectLabel(): string {
+      return !this.fetchingCountries
+        ? 'Select a country'
+        : 'Fetching country options...';
+    },
+    allowStateSelect(): boolean {
+      return this.selectedCountry !== '' && this.stateOptions.length > 0;
+    },
+    stateSelectLabel(): string {
+      return !this.fetchingStates
+        ? 'Select a state'
+        : 'Fetching state options...';
+    },
+    stateSelectMessage(): string {
+      return this.selectedCountry
+        ? 'Please Choose an Option'
+        : 'Please select a country first';
+    },
+    allowCitySelect(): boolean {
+      return this.selectedState !== '' && this.cityOptions.length > 0;
+    },
+    citySelectLabel(): string {
+      let selectCityLabel;
+      if (!this.fetchingCities) {
+        if (!this.cityOptions.length && this.selectedState !== '') {
+          selectCityLabel = 'There selected state contains no cities. Please select another state.';
+        } else {
+          selectCityLabel = 'Select a city';
+        }
+      } else {
+        selectCityLabel = 'Fetching city options...';
+      }
+      return selectCityLabel;
+    },
+    citySelectMessage(): string {
+      return this.selectedCountry
+        ? 'Please Choose an Option'
+        : 'Please select a state first';
+    },
+  },
+  mounted() {
+    this.fetchCountryOptions();
   },
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  max-width: 600px;
+  margin: auto;
+  padding: 0 2rem;
+  height: 100%;
+}
+#app > div {
+  display: flex;
+  flex-flow: column wrap;
+  align-items: flex-start;
 }
 </style>
--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="input-group">
     <label :for="name" v-if="label">{{ label }}</label>
-    <select v-model="internalValue" :name="name" :disabled="disabled">
-      <option disabled selected value="">Please Choose an Option</option>
+    <select v-model="internalValue" :name="name" :id="name" :disabled="disabled">
+      <option disabled selected value="">{{message}}</option>
       <option v-for="(option, i) in options" :key="i" :value="option">
         {{ option }}
       </option>
@@ -31,6 +31,10 @@ export default defineComponent({
       type: String,
       required: false,
     },
+    message: {
+      type: String,
+      default: 'Please Choose an Option',
+    },
     disabled: {
       type: Boolean,
       default: false,
@@ -48,3 +52,10 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="scss">
+.input-group {
+  display: inline-flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+}
+</style>
